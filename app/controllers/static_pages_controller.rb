0c77eb0625d7e7075a3979393a316a4071752d53
kei <tenguuenglish@gmail.com>
2019-04-18 18:13:43 +0900

    Create Posts ranking by number of stocks

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,20 @@ class StaticPagesController < ApplicationController
 
   def home
     @posts = Post.all.page(params[:page]).per(6)
+
+    #stockされた数が多い順に投稿を並べ替えて返す(人気の記事)
+    @stock_count_id = Stock.group(:post_id).order('count_post_id DESC').count(:post_id).keys
+    @stock_count_post_array = @stock_count_id.map{|id| Post.find(id)}
+    @stock_count_posts = Kaminari.paginate_array(@stock_count_post_array).page(params[:page]).per(6)
+
+    #検索フォームから入力されたキーワード(タイトルまたは著者)を含む記事を返す
     @search = Post.ransack(params[:q])
-    @search_posts = @search.result.page(params[:page]).per(6)
+    if params[:q].nil?
+      @search_posts = []
+    else
+      @search_posts = @search.result.page(params[:page]).per(6)
+    end
+
     @stocks = Stock.all
   end
 
